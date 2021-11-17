06aad7109119e72082b4b0a16a77b90901d94082
Sly Gryphon <sly@gamertheory.net>
2021-11-17 19:10:36 +1000

    Start based on simple example

---
+++ Examples/FactoryExample/Models/FactoryFloor.cs
@@ -0,0 +1,12 @@
+﻿using Telstra.Twins.Attributes;
+
+namespace FactoryExample.Models
+{
+    [DigitalTwin(Version = 1, DisplayName = "Digital Factory - Interface Model")]
+    public class FactoryFloor
+    {
+        [TwinProperty] public string? FloorId { get; set; }
+
+        [TwinProperty] public string? FloorName { get; set; }
+    }
+}
