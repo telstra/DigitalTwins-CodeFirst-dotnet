3ba75f501e1fc683dee50e371292af89934bc569
Sly Gryphon <sly@gamertheory.net>
2021-11-17 18:28:18 +1000

    Basic example to serialize model & twin

---
+++ Examples/SimpleExample/Models/Factory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Telstra.Twins.Attributes;
+
+namespace SimpleExample.Models
+{
+    [DigitalTwin(Version = 1, DisplayName = "Digital Factory - Interface Model")]
+    public class Factory
+    {
+        [TwinProperty] public string? Country { get; set; }
+
+        [TwinProperty] public string? FactoryId { get; set; }
+
+        [TwinProperty] public string? FactoryName { get; set; }
+
+        [TwinRelationship(DisplayName = "Has Floors")]
+        public List<FactoryFloor> Floors { get; set; } = new();
+
+        [TwinProperty] public string? ZipCode { get; set; }
+    }
+}
