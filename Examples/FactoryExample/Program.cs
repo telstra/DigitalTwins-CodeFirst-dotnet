112d12df0bc2b2b750aaa0fa36d28af38a4bb57b
Sly Gryphon <sly@gamertheory.net>
2021-11-17 22:07:03 +1000

    Add model creation option

--- Examples/FactoryExample/Program.cs
+++ Examples/FactoryExample/Program.cs
@@ -1,77 +1,153 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
 using FactoryExample.Devices;
 using FactoryExample.Models;
 using FactoryExample.Schema;
+using Microsoft.Azure.DigitalTwins.Parser;
+using Microsoft.Extensions.Configuration;
 using Telstra.Twins.Core;
 using Telstra.Twins.Services;
 
-Console.WriteLine("Simple Digital Twin Serializer Example");
+Console.WriteLine("Digital Twin Code First Factory Example");
 
-var modelLibrary = new ModelLibrary();
-var serializer = new DigitalTwinSerializer(modelLibrary);
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
 
-Console.WriteLine("Models:");
-Console.WriteLine();
-
-var modelTypes = new[]
+var show = configuration.GetValue<string>("show");
+if (!string.IsNullOrWhiteSpace(show))
 {
-    typeof(Factory), typeof(FactoryFloor), typeof(ProductionLine), typeof(ProductionStep),
-    typeof(ProductionStepGrinding)
-};
-foreach (var modelType in modelTypes)
-{
-    var modelDtdl = serializer.SerializeModel(modelType);
-    Console.WriteLine(modelDtdl);
-    Console.WriteLine();
+    switch (show.ToLowerInvariant())
+    {
+        case "model":
+            var modelTypes = new[]
+            {
+                typeof(Factory), typeof(FactoryFloor), typeof(ProductionLine), typeof(ProductionStep),
+                typeof(ProductionStepGrinding)
+            };
+            ShowModels(modelTypes);
+            return;
+        case "example":
+            ShowExamples();
+            return;
+    }
 }
 
-Console.WriteLine("Twins:");
-Console.WriteLine();
-
-var productionStepGrinding = new ProductionStepGrinding
+var create = configuration.GetValue<string>("create");
+if (!string.IsNullOrWhiteSpace(create))
 {
-    ChassisTemperature = 50,
-    FinalStep = false,
-    Force = 8.0,
-    GrindingTime = 30,
-    OperationStatus = ProductionStepStatus.Online,
-    PowerUsage = 100,
-    StartTime = new DateTimeOffset(2021, 11, 17, 19, 57, 0, TimeSpan.FromHours(10)),
-    StepId = "step1",
-    StepName = "GrindingStep"
-};
+    switch (create.ToLowerInvariant())
+    {
+        case "model":
+            var modelTypes = new[]
+            {
+                typeof(Factory), typeof(FactoryFloor), typeof(ProductionLine), typeof(ProductionStep),
+                typeof(ProductionStepGrinding)
+            };
+            await CreateModels(modelTypes);
+            return;
+    }
+}
 
-var productionLine = new ProductionLine
+ShowHelp();
+
+async Task CreateModels(Type[] modelTypes)
 {
-    CurrentProductId = "production1",
-    LineId = "line1",
-    LineName = "ProductionLine",
-    LineOperationStatus = ProductionLineStatus.Online,
-    ProductBatchNumber = 5
-};
-productionLine.RunsSteps.Add(productionStepGrinding);
+    var modelLibrary = new ModelLibrary();
+    var serializer = new DigitalTwinSerializer(modelLibrary);
 
-var factoryFloor = new FactoryFloor
+    var models = modelTypes.Select(x => serializer.SerializeModel(x));
+
+    // Validate
+    try
+    {
+        var parser = new ModelParser();
+        var entityInfos = await parser.ParseAsync(models);
+        foreach (var kvp in entityInfos)
+        {
+            Console.WriteLine($"{kvp.Key} {kvp.Value}");
+        }
+    }
+    catch (RequestFailedException ex)
+    {
+        Console.WriteLine($"Failed due to {ex}");
+    }
+}
+
+void ShowHelp()
 {
-    FloorId = "floor1", FloorName = "FactoryFloor", Temperature = 23, ComfortIndex = 0.8
-};
-factoryFloor.RunsLines.Add(productionLine);
+    Console.WriteLine(" show model - shows the serialized model");
+    Console.WriteLine(" show example - shows a serialized twin");
+    Console.WriteLine(" create model - uploads the model");
+}
 
-var factory = new Factory
+void ShowModels(Type[] modelTypes)
 {
-    FactoryId = "factory1",
-    Country = "AU",
-    ZipCode = "4000",
-    FactoryName = "Chocolate Factory",
-    GeoLocation = new GeoCord { Latitude = -27.4705, Longitude = 153.026 },
-    LastSupplyDate = new DateTimeOffset(2021, 11, 17, 18, 37, 0, TimeSpan.FromHours(10))
-};
-factory.Floors.Add(factoryFloor);
+    var modelLibrary = new ModelLibrary();
+    var serializer = new DigitalTwinSerializer(modelLibrary);
 
-var twin1Dtdl = serializer.SerializeTwin(factory);
-Console.WriteLine(twin1Dtdl);
-Console.WriteLine();
+    foreach (var modelType in modelTypes)
+    {
+        var modelDtdl = serializer.SerializeModel(modelType);
+        Console.WriteLine(modelDtdl);
+        Console.WriteLine();
+    }
+}
 
-var twin2Dtdl = serializer.SerializeTwin(factory.Floors[0].RunsLines[0].RunsSteps[0]);
-Console.WriteLine(twin2Dtdl);
-Console.WriteLine();
+void ShowExamples()
+{
+    var modelLibrary = new ModelLibrary();
+    var serializer = new DigitalTwinSerializer(modelLibrary);
+
+    var productionStepGrinding = new ProductionStepGrinding
+    {
+        ChassisTemperature = 50,
+        FinalStep = false,
+        Force = 8.0,
+        GrindingTime = 30,
+        OperationStatus = ProductionStepStatus.Online,
+        PowerUsage = 100,
+        StartTime = new DateTimeOffset(2021, 11, 17, 19, 57, 0, TimeSpan.FromHours(10)),
+        StepId = "step1",
+        StepName = "GrindingStep"
+    };
+
+    var productionLine = new ProductionLine
+    {
+        CurrentProductId = "production1",
+        LineId = "line1",
+        LineName = "ProductionLine",
+        LineOperationStatus = ProductionLineStatus.Online,
+        ProductBatchNumber = 5
+    };
+    productionLine.RunsSteps.Add(productionStepGrinding);
+
+    var factoryFloor = new FactoryFloor
+    {
+        FloorId = "floor1", FloorName = "FactoryFloor", Temperature = 23, ComfortIndex = 0.8
+    };
+    factoryFloor.RunsLines.Add(productionLine);
+
+    var factory = new Factory
+    {
+        FactoryId = "factory1",
+        Country = "AU",
+        ZipCode = "4000",
+        FactoryName = "Chocolate Factory",
+        GeoLocation = new GeoCord { Latitude = -27.4705, Longitude = 153.026 },
+        LastSupplyDate = new DateTimeOffset(2021, 11, 17, 18, 37, 0, TimeSpan.FromHours(10))
+    };
+    factory.Floors.Add(factoryFloor);
+
+    var twin1Dtdl = serializer.SerializeTwin(factory);
+    Console.WriteLine(twin1Dtdl);
+    Console.WriteLine();
+
+    var twin2Dtdl = serializer.SerializeTwin(factory.Floors[0].RunsLines[0].RunsSteps[0]);
+    Console.WriteLine(twin2Dtdl);
+    Console.WriteLine();
+}
