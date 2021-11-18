fcd2a6a67858775712dfe75992d427f7e7bb9b1f
Sly Gryphon <sly@gamertheory.net>
2021-11-18 20:43:33 +1000

    Basic test for simple telemetry

---
+++ Tests/Telstra.Twins.Test/TwinTelemetryAttributeTests.cs
@@ -0,0 +1,40 @@
+﻿using Telstra.Twins.Attributes;
+using Telstra.Twins.Core;
+using Telstra.Twins.Services;
+using Xunit;
+
+namespace Telstra.Twins.Test
+{
+    public class TwinTelemetryAttributeTests
+    {
+        public TwinTelemetryAttributeTests()
+        {
+            var modelLibrary = new ModelLibrary();
+            Serializer = new DigitalTwinSerializer(modelLibrary);
+        }
+
+        private DigitalTwinSerializer Serializer { get; }
+
+        [Fact]
+        public void BasicTelemetryShouldSerialiseToModel()
+        {
+            var expectedModel = @"{
+""@id"": ""dtmi:telstra:twins:test:twinwithtelemetry;1"",
+""@type"": ""Interface"",
+""@context"": ""dtmi:dtdl:context;2"",
+""contents"": [
+    {  ""@type"": ""Telemetry"", ""name"": ""measurement"", ""schema"": ""integer"" }
+]}";
+
+            var model = Serializer.SerializeModel(typeof(TwinWithTelemetry));
+
+            JsonAssert.Equal(expectedModel, model);
+        }
+
+        [DigitalTwin]
+        private class TwinWithTelemetry : TwinBase
+        {
+            [TwinTelemetry] public int Measurement { get; set; }
+        }
+    }
+}
