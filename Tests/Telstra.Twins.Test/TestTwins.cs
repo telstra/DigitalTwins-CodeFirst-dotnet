709adf9a31e5749275c876db9ba0f566cb7e985f
Mahdi GhorbanPour <mahdi.ghorbanpour@ptwdosimetry.com>
2021-12-01 09:05:56 +0100

    Fixed the unit tests

--- Tests/Telstra.Twins.Test/TestTwins.cs
+++ Tests/Telstra.Twins.Test/TestTwins.cs
@@ -1,39 +1,65 @@
-using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
-using System.Collections.Generic;
-using Telstra.Twins;
+﻿using System.Collections.Generic;
 using Telstra.Twins.Attributes;
-using Telstra.Twins.Models;
-using Azure.DigitalTwins.Core;
+using System;
 
 namespace Telstra.Twins.Test
 {
-    [DigitalTwin(Version=1, DisplayName="Simple Twin")]
-    public class SimpleTwin: TwinBase
+    public abstract class Space : TwinBase
+    {
+        [TwinProperty]
+        public string Name { get; set; }
+    }
+
+
+    [DigitalTwin(Version = 1, DisplayName = "Building")]
+    public class Building : Space
+    {
+        [TwinProperty]
+        public DateTimeOffset ConstructionDate { get; set; }
+
+        [TwinProperty]
+        public int? RoomsCount { get; set; }
+
+        [TwinRelationship(Name = "contains")]
+        public List<Floor> Floors { get; set; }
+
+        [TwinRelationship(Name = "otherPlaces")]
+        public List<Space> OtherSpaces { get; set; }
+    }
+
+    [DigitalTwin(Version = 1, DisplayName = "Floor")]
+    public class Floor : Space
+    {
+        [TwinProperty]
+        public decimal? Temperator { get; set; }
+    }
+
+    [DigitalTwin(Version = 1, DisplayName = "Simple Twin")]
+    public class SimpleTwin : TwinBase
     {
         [TwinProperty]
         public int Quantity { get; set; }
+
         [TwinTelemetry]
         public int Measurement { get; set; }
     }
 
-    [DigitalTwin(Version=1,DisplayName="Twin with a relationship")]
-    public class TwinWithRelationship: TwinBase
+    [DigitalTwin(Version = 1, DisplayName = "Twin with a relationship")]
+    public class TwinWithRelationship : TwinBase
     {
 
         [TwinProperty]
         public int Count { get; set; }
 
-        [TwinRelationship(Name = "Test Relationship", MaxMultiplicity = 5)]
-        public SimpleTwin Relationship { get; set;} 
-
+        [TwinRelationship(Name = "TestRelationship", MaxMultiplicity = 5)]
+        public SimpleTwin Relationship { get; set; }
     }
 
-    [DigitalTwin(Version=1,DisplayName="Twin with a nested object")]
-    public class TwinWithNestedObject: TwinBase
+    [DigitalTwin(Version = 1, DisplayName = "Twin with a nested object")]
+    public class TwinWithNestedObject : TwinBase
     {
         public TwinWithNestedObject()
-        {}
+        { }
         [TwinProperty]
         public NestedObject NestedObj { get; set; }
 
@@ -44,7 +70,7 @@ namespace Telstra.Twins.Test
     public class NestedObject
     {
         public NestedObject()
-        {}
+        { }
 
         public string name { get; set; }
         public string value { get; set; }
@@ -72,7 +98,7 @@ namespace Telstra.Twins.Test
         [TwinProperty]
         public Dictionary<string, string> StringMap { get; set; }
 
-}
+    }
     public static class DataGenerator
     {
         public static TwinWithNestedObject twinWithNestedObject = new TwinWithNestedObject()
@@ -109,8 +135,8 @@ namespace Telstra.Twins.Test
             Property = "property",
             Flag = true,
             ComponentTwin = simpleTwin,
-            IntArray = new List<int>() {1 ,2, 3},
-            StringMap = new Dictionary<string, string>() 
+            IntArray = new List<int>() { 1, 2, 3 },
+            StringMap = new Dictionary<string, string>()
             {
                 {"key","value"}
             }
@@ -121,7 +147,7 @@ namespace Telstra.Twins.Test
         public static string TwinWithNestedObjectDTDL = "{\r\n  \"$dtId\": \"11111\",\r\n  \"$etag\": \"abcd\",\r\n  \"$metadata\": {\r\n    \"$model\": \"dtmi:telstra:twins:test:twinwithnestedobject;1\",\r\n    \"PropertyMetadata\": {}\r\n  },\r\n  \"speed\": 50,\r\n  \"nestedObj\": {\r\n    \"name\": \"name\",\r\n    \"value\": \"value\"\r\n  }\r\n}";
         public static string TwinWithNestedObjectModel = "{\r\n  \"@id\": \"dtmi:telstra:twins:test:twinwithnestedobject;1\",\r\n  \"@type\": \"Interface\",\r\n  \"@context\": \"dtmi:dtdl:context;2\",\r\n  \"displayName\": \"Twin with a nested object\",\r\n  \"contents\": [\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"nestedObj\",\r\n      \"schema\": {\r\n        \"@type\": \"Object\",\r\n        \"fields\": [\r\n          {\r\n            \"name\": \"name\",\r\n            \"schema\": \"string\"\r\n          },\r\n          {\r\n            \"name\": \"value\",\r\n            \"schema\": \"string\"\r\n          }\r\n        ]\r\n      }\r\n    },\r\n    {\r\n      \"@type\": \"Telemetry\",\r\n      \"name\": \"speed\",\r\n      \"schema\": \"integer\"\r\n    }\r\n  ]\r\n}";
         public static string TwinWithRelationshipDTDL = "    {\r\n  \"$dtId\": \"1221\",\r\n  \"$etag\": \"Etag\",\r\n  \"$metadata\": {\r\n    \"$model\": \"dtmi:telstra:twins:test:twinwithrelationship;1\",\r\n    \"PropertyMetadata\": {}\r\n  },\r\n  \"count\": 10\r\n}";
-        public static string TwinWithRelationshipModel = "{\r\n  \"@id\": \"dtmi:telstra:twins:test:twinwithrelationship;1\",\r\n  \"@type\": \"Interface\",\r\n  \"@context\": \"dtmi:dtdl:context;2\",\r\n  \"displayName\": \"Twin with a relationship\",\r\n  \"contents\": [\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"count\",\r\n      \"schema\": \"integer\"\r\n    },\r\n    {\r\n      \"maxMultiplicity\": 5,\r\n      \"minMultiplicity\": 0,\r\n      \"target\": \"dtmi:telstra:twins:test:simpletwin;1\",\r\n      \"@type\": \"Relationship\",\r\n      \"name\": \"relationship\"\r\n    }\r\n  ]\r\n}";
+        public static string TwinWithRelationshipModel = "{\r\n  \"@id\": \"dtmi:telstra:twins:test:twinwithrelationship;1\",\r\n  \"@type\": \"Interface\",\r\n  \"@context\": \"dtmi:dtdl:context;2\",\r\n  \"displayName\": \"Twin with a relationship\",\r\n  \"contents\": [\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"count\",\r\n      \"schema\": \"integer\"\r\n    },\r\n    {\r\n      \"maxMultiplicity\": 5,\r\n      \"target\": \"dtmi:telstra:twins:test:simpletwin;1\",\r\n      \"@type\": \"Relationship\",\r\n      \"name\": \"testRelationship\"\r\n    }\r\n  ]\r\n}";
         public static string TwinWithAllAttributesDTDL = "{\r\n  \"$dtId\": \"0\",\r\n  \"$etag\": \"0\",\r\n  \"$metadata\": {\r\n    \"$model\": \"dtmi:telstra:twins:test:twinwithallattributes;1\",\r\n    \"PropertyMetadata\": {}\r\n  },\r\n  \"flag\": true,\r\n  \"testProperty\": \"property\",\r\n  \"intArray\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"stringMap\": {\r\n    \"key\": \"value\"\r\n  },\r\n  \"componentTwin\": {\r\n    \"$dtId\": \"122233\",\r\n    \"$etag\": \"4444\",\r\n    \"$metadata\": {\r\n      \"$model\": \"dtmi:telstra:twins:test:simpletwin;1\",\r\n      \"PropertyMetadata\": {}\r\n    },\r\n    \"measurement\": 2,\r\n    \"quantity\": 1\r\n  }\r\n}";
         public static string TwinWithAllAttributesModel = "    {\r\n  \"@id\": \"dtmi:telstra:twins:test:twinwithallattributes;1\",\r\n  \"@type\": \"Interface\",\r\n  \"@context\": \"dtmi:dtdl:context;2\",\r\n  \"displayName\": \"Twin with all attributes\",\r\n  \"contents\": [\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"property\",\r\n      \"schema\": \"string\"\r\n    },\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"intArray\",\r\n      \"schema\": {\r\n        \"@type\": \"Array\",\r\n        \"elementSchema\": \"integer\"\r\n      }\r\n    },\r\n    {\r\n      \"@type\": \"Property\",\r\n      \"name\": \"stringMap\",\r\n      \"schema\": {\r\n        \"@type\": \"Map\",\r\n        \"mapKey\": {\r\n          \"name\": \"name\",\r\n          \"schema\": \"string\"\r\n        },\r\n        \"mapValue\": {\r\n          \"name\": \"name\",\r\n          \"schema\": \"string\"\r\n        }\r\n      }\r\n    },\r\n    {\r\n      \"maxMultiplicity\": 1,\r\n      \"minMultiplicity\": 2,\r\n      \"target\": \"dtmi:telstra:twins:test:twinwithnestedobject;1\",\r\n      \"@type\": \"Relationship\",\r\n      \"name\": \"twinRelationship\"\r\n    },\r\n    {\r\n      \"@type\": \"Component\",\r\n      \"name\": \"simpleTwin\",\r\n      \"schema\": \"dtmi:telstra:twins:test:simpletwin;1\"\r\n    },\r\n    {\r\n      \"@type\": \"Telemetry\",\r\n      \"name\": \"flag\",\r\n      \"schema\": \"boolean\"\r\n    }\r\n  ]\r\n}";
     }
